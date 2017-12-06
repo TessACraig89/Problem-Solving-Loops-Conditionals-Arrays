5a5c267d3b67e338682e8b6c8053091c875838df
Tess Craig <tessashleycraig89@gmail.com>
2017-12-06 11:53:40 -0600

    Answers to Conceptual Questions

---
+++ morning_lab_solutions.js
@@ -0,0 +1,93 @@
+// 1. How do we assign a value to a variable?
+// const variable = value;
+// let variable = value;
+
+// 2. How do we change the value of a variable?
+// variable = new value;
+
+// 3. How do we assign an existing variable to a new variable?
+// const newVariable = existingVariable
+
+///////////////////////////////////////////////////
+
+//Write the answers to your String exercises here:
+
+
+
+
+
+
+
+
+
+// Write the answers to your Boolean exercises here:
+
+
+
+
+
+
+
+
+
+// Write the answers to The Farm here:
+
+
+
+
+
+
+
+
+
+// Write the answers to Driver's Ed here:
+
+
+
+
+
+
+
+
+
+// Write the answers to Let's get even here:
+
+
+
+
+
+
+
+
+
+// Write the answers to Give me Five here:
+
+
+
+
+
+
+
+
+
+// Write the answers to Savings account here:
+
+
+
+
+
+
+
+
+
+// Write the answers to Multiples of 3 and 5 here:
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////
